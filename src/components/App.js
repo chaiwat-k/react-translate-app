b9d40d5856c1934966f8ebbc811a4efef58daa35
Chaiwat Kanchanarat <Chaiwat_K@banpu.co.th>
2018-12-30 08:54:35 +0700

    1st commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    render(){
+        return (
+            <div className="ui container">
+                <h1>App</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
