483feacba649db61aae5838dd0f061f59f28f13a
Chaiwat Kanchanarat <Chaiwat_K@banpu.co.th>
2018-12-30 11:46:31 +0700

    Use Context as App data store

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component{
 
     render(){
         return (
             <div className="ui container">
-                <h1>App</h1>
+                <LanguageStore>
+                    <LanguageSelector />
+                    <ColorContext.Provider value="primary">                
+                        <UserCreate />                   
+                    </ColorContext.Provider>
+                </LanguageStore>
             </div>
         );
     }
